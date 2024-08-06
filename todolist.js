f013e84e40b3469e34e2108c3d02275e28a3dd4d
MehakFaheem <168015629+MehakFaheem@users.noreply.github.com>
2024-08-06 20:26:22 +0500

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,17 +1,24 @@
+#! /usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var inquirer_1 = require("inquirer");
 var todos = [];
-var addtask = await inquirer_1.default.prompt([
-    {
-        name: 'todo',
-        type: 'input',
-        Message: "What do you want to add in your todos?"
-    },
-    {
-        name: 'addmore',
-        type: 'confirm',
-        Message: "Do you want to add more?"
-    }
-]);
-console.log(addtask.todo);
+var condition = true;
+while (condition) {
+    var addtask = await inquirer_1.default.prompt([
+        {
+            name: 'todo',
+            type: 'input',
+            Message: "What do you want to add in your todos?"
+        },
+        {
+            name: 'addmore',
+            type: 'confirm',
+            Message: "Do you want to add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(addtask.todo);
+    condition = addtask.addmore;
+    console.log(todos);
+}
